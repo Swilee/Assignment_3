c742d2209e2be99b060507f8f5ae04c7ec36f7c1
henkeedman <henkeedman@users.noreply.github.com>
2018-02-22 11:49:26 +0100

    Lattjat lite

--- Assignment_3_Andreasson_Edman.py
+++ Assignment_3_Andreasson_Edman.py
@@ -2,3 +2,36 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 import sys
+qt_app = QApplication(sys.argv)
+
+
+class PokerWindow(QGroupBox):
+    ''' PokerWindow represents the five cards put on the table and the player options '''
+    def __init__(self):
+        super().__init__("Poker content")
+
+        checkbutton = QPushButton("Check/Call")
+        foldbutton = QPushButton("Fold")
+        betbutton = QPushButton("Bet")
+
+        hbox = QHBoxLayout()
+
+        hbox.addStretch(1)
+        hbox.addWidget(checkbutton)
+        hbox.addWidget(foldbutton)
+        hbox.addWidget(betbutton)
+
+        vbox = QVBoxLayout()
+        vbox.addStretch(1)
+        vbox.addWidget(QLabel('Buttons:'))
+        vbox.addLayout(hbox)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(600, 300, 300, 150)
+        self.setWindowTitle('Texas Holdem')
+
+
+game = PokerWindow()
+game.show()
+qt_app.exec_()
