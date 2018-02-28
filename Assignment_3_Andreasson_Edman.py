b2c01fcaad0bb9ce36efc6cecc5b0df9beacc4ed
Swilee <aleandre@student.chalmers.se>
2018-02-28 15:58:40 +0100

    inte så snyggt men

--- Assignment_3_Andreasson_Edman.py
+++ Assignment_3_Andreasson_Edman.py
@@ -9,7 +9,7 @@ class PokerWindow(QGroupBox):
     ''' PokerWindow represents the five cards put on the table and the player options '''
     def __init__(self):
         super().__init__("Poker content")
-        pot=QGroupBox('pot=%d' %123451)
+        pot = QGroupBox('pot=%d' % 123451)
         checkbutton = QPushButton("Check/Call")
         foldbutton = QPushButton("Fold")
         betbutton = QPushButton("Bet")
@@ -36,7 +36,32 @@ class PokerWindow(QGroupBox):
         hbox.addStretch(1)
         hbox.addLayout(vbox)
 
-        self.setLayout(hbox)
+        player1box = QHBoxLayout()
+        player1card1 = QPushButton("första")
+        player1card2 = QPushButton("andra")
+
+        player2card1 = QPushButton("första")
+        player2card2 = QPushButton("andra")
+
+        Playername1 = QGroupBox('Player: %s' % 'Alex')
+        Playername2 = QGroupBox('Player: %s' % 'Edman')
+
+        player1box.addWidget(player2card1)
+        player1box.addWidget(player2card2)
+        player1box.addWidget(Playername1)
+
+        player2box = QHBoxLayout()
+        player2box.addWidget(player1card1)
+        player2box.addWidget(player1card2)
+        player2box.addWidget(Playername2)
+
+
+        final=QVBoxLayout()
+        final.addLayout(hbox)
+        final.addLayout(player1box)
+        final.addLayout(player2box)
+
+        self.setLayout(final)
         self.setGeometry(600, 300, 300, 150)
         self.setWindowTitle('Texas Holdem')
 
